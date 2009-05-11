091772253e4a667b64b7542ca55909890fbaa681
yibble <yibble@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-11 22:54:51 +0000

    GnuPG shaping up
    git-svn-id: https://plugins.svn.wordpress.org/web-tripwire/trunk@117267 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/wp-gnupg.php
+++ includes/wp-gnupg.php
@@ -54,7 +54,6 @@ function verify_signing_key () {
 		$info = gnupg_import( $cryptoshell, $keydata )
 			or die ( "Unable to import public key. Key ID = 1C1DC95C" );
 		var_dump ($info);
-		var_dump ($keydata);
 	}	
 }
 
